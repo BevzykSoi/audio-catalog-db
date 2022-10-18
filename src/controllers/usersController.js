a80c5c5b6a738d0c745a0c8bdbbfa798fca706e1
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-10-18 16:45:06 +0200

    Made a template for routes of users

---
+++ src/controllers/usersController.js
@@ -0,0 +1,42 @@
+exports.getUser = async (req, res, next) => {
+    try {
+       
+    } catch (error) {
+       next(error);
+    }
+}
+
+exports.getUserAudios = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getUserFollowings = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getUserFollowers = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.followUser = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.unfollowUser = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const authRouter = require('./authRouter');
+const usersRouter = require('./usersRouter');
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
