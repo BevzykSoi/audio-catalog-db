a5f4151b45bedc17afc2d9a37146b405151ba8fb
Anna-Dominika1 <koznadia53@gmail.com>
2022-11-08 18:51:42 +0200

    audio-routes

--- src/routes/audiosRouter.js
+++ src/routes/audiosRouter.js
@@ -2,13 +2,14 @@ const express = require('express');
 const audioController = require('../controllers/audioController');
 const path = require('path');
 const router = express.Router();                                                                                
-                                                                                                                                                                                                                                                                                                                        
+const Audio = require('../models/Audio')                                                                                                                                                                                                                                                                                                                       
 const multer = require('multer');                                                                                 
 const fs = require('fs').promises;
 const Jimp = require('jimp');
 const { number } = require('yup');
 const { string } = require('yup/lib/locale');
-const imagesPath = path.join(process.cwd(), 'public/images');
+const auth = require('../middlewares/auth')
+// const imagesPath = path.join(process.cwd(), 'public/images');
 const audiosPath = path.join(process.cwd(), 'public/audios')
 const uploadsPath = path.join(process.cwd(), 'public/uploads');
 const staticFolderPath = path.join(process.cwd(), 'public');
@@ -28,20 +29,21 @@ const upload = multer({
   storage,
 });
 router.post(
-  '/',
+  '/',auth,
   upload.fields([
     { name: 'audio', maxCount: 1 },
     { name: 'cover', maxCount: 1 },
   ]),
-  async (req, res) => {
+  async (req, res, next) => {
+    try{
     console.log(req.file);
 
-    const audioPath = path.join(audiosPath, req.files.audio[0].path);
-    const coverPath = path.join(audiosPath, req.files.cover[0].path);
+    const audioPath =  req.files.audio[0].path;
+    const coverPath =  req.files.cover[0].path;
  
-    const audioUploadResponse = await cloudinary.v2.uploader.upload(audioPath, { resource_type: 'video' });
-    const coverUploadResponse = await cloudinary.v2.uploader.upload(coverPath);
-
+    const audioUploadResponse = await cloudinary.uploader.upload(audioPath, { resource_type: 'video' });
+    const coverUploadResponse = await cloudinary.uploader.upload(coverPath);
+    console.log(req.files.audio[0].path);
     await fs.unlink(audioPath);
     await fs.unlink(coverPath);
 
@@ -51,20 +53,23 @@ router.post(
       author: req.user._id,
       duration: req.body.duration,
       fileUrl: audioUploadResponse.secure_url,
-      coverUrl: coverUploadResponse.secure_url}) /
+      coverUrl: coverUploadResponse.secure_url}) 
     req.user.createdAudios.push(newAudio._id)
     await req.user.save();
     await newAudio.populate('author');
 
     res.json(newAudio);
+  }catch(error){
+    console.log(error)
+  }
   }
 );//
 router.get('/', audioController.getAll);//
 
-router.get('/:id', audioController.getById);//
+
 router.patch('/:id/favorite', audioController.favorite);//
 router.get('/top', audioController.getAllTop);//
 router.get('/new', audioController.getAllNew);//
 router.delete('/:id', audioController.delete);//
-
+router.get('/:id', audioController.getById);// 
 module.exports = router;
