a2c85c3bdb8f1ded4d62340b69ef7b57e0f33cb6
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-11-21 18:46:14 +0100

    Tried to add genres route (Nazarenko don't pull)

--- src/routes/audiosRouter.js
+++ src/routes/audiosRouter.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const Audio = require('../models/Audio');
+
+const { Audio, Profile } = require('../models');
+
 const multer = require('multer');
 const fs = require('fs').promises;
 const Jimp = require('jimp');
 const { number } = require('yup');
 const { string } = require('yup/lib/locale');
+
 const { auth, historyAuth } = require('../middlewares');
+
 const audiosPath = path.join(process.cwd(), 'public/audios');
 const audioController = require('../controllers/audioController');
 
 const cloudinary = require('cloudinary').v2;
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, audiosPath);
@@ -34,8 +39,6 @@ router.post(
   ]),
   async (req, res, next) => {
     try {
-      console.log(req.file);
-
       const audioPath = req.files.audio[0].path;
       const coverPath = req.files.cover[0].path;
 
@@ -43,9 +46,23 @@ router.post(
         resource_type: 'video',
       });
       const coverUploadResponse = await cloudinary.uploader.upload(coverPath);
-      console.log(req.files.audio[0].path);
+
       await fs.unlink(audioPath);
       await fs.unlink(coverPath);
+
+      await req.user.populate('profile');
+
+      req.user.profile = await Profile.findByIdAndUpdate(
+        req.user._id,
+        {
+          $addToSet: {
+            genres: { $each: req.body.genres.split(', ') },
+          },
+        },
+        {
+          new: true,
+        }
+      );
 
       const newAudio = await Audio.create({
         name: req.body.name,
